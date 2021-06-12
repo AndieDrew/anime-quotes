f0159e3e7717152c2a57d6691330f919c83e8e25
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-11 23:37:11 -0400

    Create initial QuoteArea that returns Future Quote

--- src/components/QuoteArea/QuoteArea.js
+++ src/components/QuoteArea/QuoteArea.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './QuoteArea.css'
 
-
+const QuoteArea = () => {
+    return (
+        <div className = 'Area'>
+            <h1>Future Quote</h1>
+        </div>
+    )
+}
 
 export default QuoteArea
