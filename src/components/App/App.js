7c885507c30d1852bcb59387db79f3e20b0c0f2e
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-12 20:15:18 -0400

    Refactor App pass fetched quote as prop to QuoteArea

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from '../Header/Header'
 import QuoteArea from '../QuoteArea/QuoteArea'
-
+import { getQuote } from '../../util/api-calls'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      animeQuote: null,
     }
   }
 
   componentDidMount() {
-
+    getQuote()
+      .then(data => {
+        this.setState({animeQuote: data})
+      })
   }
 
   render() {
@@ -25,7 +28,11 @@ class App extends Component {
           <Switch>
             <Route exact path ='/'
               render={() => (
-                <QuoteArea/>
+                !this.state.animeQuote ?
+                <h1>Loading</h1>
+
+                : this.state.animeQuote &&
+                <QuoteArea animeQuote={ this.state.animeQuote }/>
               )}
               />
           </Switch>
