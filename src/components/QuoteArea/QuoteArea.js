a0284a2d17466ede1e0724f5dfc72235aea902e3
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-12 20:15:51 -0400

    Pass quote to QuoteCard from QuoteArea as a prop

--- src/components/QuoteArea/QuoteArea.js
+++ src/components/QuoteArea/QuoteArea.js
@@ -2,10 +2,10 @@ import React from 'react'
 import QuoteCard from '../QuoteCard/QuoteCard'
 import './QuoteArea.css'
 
-const QuoteArea = () => {
+const QuoteArea = ( {animeQuote} ) => {
     return (
         <div className = 'Area'>
-            <QuoteCard />
+            <QuoteCard animeQuote={animeQuote}/>
         </div>
     )
 }
