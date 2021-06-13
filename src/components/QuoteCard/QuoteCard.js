fac2eea3f74177ef0c6fb92ef2976700be9a8007
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-13 18:21:02 -0400

    Wrap card article in a div container

--- src/components/QuoteCard/QuoteCard.js
+++ src/components/QuoteCard/QuoteCard.js
@@ -4,13 +4,13 @@ import './QuoteCard.css'
 const QuoteCard =( props ) => {
 const { anime, character, quote } = props.animeQuote
     return (
-      
+      <div className='cardContainer'>
         <article className='card'>
           <h1 className='animeTitle'>{anime}</h1>
           <h2 className='animeQuote'>{quote}</h2>
           <h2 className='animeCharacter'>- {character}</h2>
         </article>
-
+      </div>
     )
   }
 
