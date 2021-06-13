07c3eed6bba3df326ceab054cb54d9fbececef76
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-12 21:07:49 -0400

    Add classnames to quoteCar elements for testing & styling

--- src/components/QuoteCard/QuoteCard.js
+++ src/components/QuoteCard/QuoteCard.js
@@ -5,9 +5,9 @@ const QuoteCard =( props ) => {
 const { anime, character, quote } = props.animeQuote
     return (
       <div className='card'>
-        <h1>{anime}</h1>
-        <h2>{quote}</h2>
-        <h2>- {character}</h2>
+        <h1 className='animeTitle'>{anime}</h1>
+        <h2 className='animeQuote'>{quote}</h2>
+        <h2 className='animeCharacter'>- {character}</h2>
       </div>
     )
   }
