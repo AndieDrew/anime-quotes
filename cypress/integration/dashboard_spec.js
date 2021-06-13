951c0b4be479cf9e198262f1140ebf91c823c0b7
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-12 21:04:35 -0400

    Create it block testing header elements

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -2,5 +2,10 @@ describe('Dashboard', () => {
     beforeEach(() => {
       cy.load()
     });
-
+    
+    it('Should show application title', () => {
+        cy.get('.header').should('be.visible')
+          .get('.pageTitle').should('be.visible')
+          .get('.header > span > .pageTitle').contains('Anime Quotes')
+    })
 });
