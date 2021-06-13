df6cc45e539aeeb34feb3f80dd7fd7bf81c8a7ad
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-12 21:18:09 -0400

    Add testing for dashboard quote card

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,11 +1,23 @@
 describe('Dashboard', () => {
     beforeEach(() => {
-      cy.load()
+      
     });
-    
+
     it('Should show application title', () => {
+        cy.load()
         cy.get('.header').should('be.visible')
           .get('.pageTitle').should('be.visible')
           .get('.header > span > .pageTitle').contains('Anime Quotes')
+    })
+
+    it('Should show a quote card with correct data', () => {
+        cy.load()
+        cy.get('.card').should('be.visible')
+          .get('.card > .animeTitle').should('be.visible')
+          .get('.card > .animeQuote').should('be.visible')
+          .get('.card > .animeCharacter').should('be.visible')
+          .get('.card > .animeTitle').contains('Fairy Tail')
+          .get('.card > .animeQuote').contains('If you don\'t have the courage to change')
+          .get('.card > .animeCharacter').contains('- Natsu Dragneel')
     })
 });
