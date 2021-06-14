bb83a48cbcd224c2ea3ac0641864ed044a6e42b0
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-13 21:10:08 -0400

    Create favorites class component skelaton

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,21 @@
+class Favorites extends Component {
+    constructor() {
+      super()
+      this.state = {
+  
+      }
+    }
+  
+    componentDidMount() {
+  
+    }
+  
+    render() {
+      return (
+  
+           
+      )
+    }
+  }
+  
+  export default Favorites
