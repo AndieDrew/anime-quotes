19ffc31dd9498776855d136a2b44cfc841e7a64e
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 19:31:48 -0400

    Test that favorites button navigates away from home page

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -35,4 +35,27 @@ describe('Dashboard', () => {
           .get('.card > .animeQuote').contains('Test Quote')
           .get('.card > .animeCharacter').contains('Test Character')
     })
+
+    it('Should navigate away from home page', () => {
+        cy.load()
+            .get('.header').should('be.visible')
+            .get('.pageTitle').should('be.visible')
+            .get('.header > .pageTitle').contains('Anime Quotes')
+            .get('.card').should('be.visible')
+            .get('.card > .animeTitle').should('be.visible')
+            .get('.card > .animeQuote').should('be.visible')
+            .get('.card > .animeCharacter').should('be.visible')
+            .get('.card > .animeTitle').contains('Fairy Tail')
+            .get('.card > .animeQuote').contains('If you don\'t have the courage to change')
+            .get('.card > .animeCharacter').contains('- Natsu Dragneel')
+
+            .get('li').eq(1).click()
+            .get('.header').should('be.visible')
+            .get('.pageTitle').should('be.visible')
+            .get('.header > .pageTitle').contains('Anime Quotes')
+            .get('.card').should('not.exist')
+            .get('.card > .animeTitle').should('not.exist')
+            .get('.card > .animeQuote').should('not.exist')
+            .get('.card > .animeCharacter').should('not.exist')
+    })
 });
