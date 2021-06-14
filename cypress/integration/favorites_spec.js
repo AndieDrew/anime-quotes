0d549f1da0424080960d9ff213e5a48394d33917
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 19:48:32 -0400

    Test that favorite quote is displayed on favorites page

--- cypress/integration/favorites_spec.js
+++ cypress/integration/favorites_spec.js
@@ -10,6 +10,20 @@ describe('Dashboard', () => {
           .get('.noFavorites').contains('You have no favorites!')
     })
 
+    it('Should show user their favorites', () => {
+        cy.load()
+          .get('.favoriteBtn').click()  
 
+          .get('li').eq(1).click()
+          .get('.noFavorites').should('not.exist')
+
+          .get('.card').should('be.visible')
+          .get('.card > .animeTitle').should('be.visible')
+          .get('.card > .animeQuote').should('be.visible')
+          .get('.card > .animeCharacter').should('be.visible')
+          .get('.card > .animeTitle').contains('Fairy Tail')
+          .get('.card > .animeQuote').contains('If you don\'t have the courage to change')
+          .get('.card > .animeCharacter').contains('- Natsu Dragneel')
+    })
 
 });
