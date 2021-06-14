234ae8e273a14b1e57e09d067cfd77152afd68ca
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 19:44:55 -0400

    Refactor test to check header is still visible

--- cypress/integration/favorites_spec.js
+++ cypress/integration/favorites_spec.js
@@ -2,13 +2,14 @@ describe('Dashboard', () => {
 
     it('Should let user know they have no favorites', () => {
         cy.load()
-        cy.get('.header').should('be.visible')
+          .get('li').eq(1).click()
+          .get('.header').should('be.visible')
           .get('.pageTitle').should('be.visible')
           .get('.header > .pageTitle').contains('Anime Quotes')
-
-          .get('li').eq(1).click()
           .get('.noFavorites').should('be.visible')
           .get('.noFavorites').contains('You have no favorites!')
     })
+
+
 
 });
