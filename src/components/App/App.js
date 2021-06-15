c21f8ffbb46dc7670356e9c0f123d346bea4341e
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 21:08:02 -0400

    Format App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,69 +1,80 @@
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Header from '../Header/Header'
-import QuoteArea from '../QuoteArea/QuoteArea'
-import Favorites from '../Favorites/Favorites'
-import { getQuote } from '../../util/api-calls'
-import './App.css'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "../Header/Header";
+import QuoteArea from "../QuoteArea/QuoteArea";
+import Favorites from "../Favorites/Favorites";
+import { getQuote } from "../../util/api-calls";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       animeQuote: null,
       favorites: [],
-      error: null
-    }
+      error: null,
+    };
   }
 
   componentDidMount() {
     getQuote()
-      .then(data => {
-        this.setState({animeQuote: data})
+      .then((data) => {
+        this.setState({ animeQuote: data });
       })
-      .catch(error => this.setState({error: error}))
+      .catch((error) => this.setState({ error: error }));
   }
 
   addToFavorites() {
-    if(!this.state.favorites.includes(this.state.animeQuote)){
-      this.setState({favorites: [...this.state.favorites, this.state.animeQuote]})
+    if (!this.state.favorites.includes(this.state.animeQuote)) {
+      this.setState({
+        favorites: [...this.state.favorites, this.state.animeQuote],
+      });
     }
   }
 
-
   render() {
     return (
-
-      <main className = 'App'>
-        <Header/>
-          <Switch>
-            <Route exact path ='/'
-              render={() => (
-                !this.state.animeQuote && !this.state.error ?
-                <h1 className='loading' >Loading</h1>
-
-                : this.state.error ?
-                <h1 className='error'>Something went wrong!</h1>
-
-                : this.state.animeQuote &&
-                <div>
-                  <QuoteArea animeQuote={ this.state.animeQuote }/>
-                  <button className='newQuoteBtn' onClick={ () => this.componentDidMount()}>New Quote</button>
-                  <button className='favoriteBtn' onClick={ () => this.addToFavorites()}>Favorite</button>
-                </div>
-              )}
-              />
-              <Route exact path ='/favorites'
-              render={() => (
-                <Favorites favoritesArray={ this.state.favorites }/>
-              )}
-              />
-          </Switch>
+      <main className="App">
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() =>
+              !this.state.animeQuote && !this.state.error ? (
+                <h1 className="loading">Loading</h1>
+              ) : this.state.error ? (
+                <h1 className="error">Something went wrong!</h1>
+              ) : (
+                this.state.animeQuote && (
+                  <div>
+                    <QuoteArea animeQuote={this.state.animeQuote} />
+                    <button
+                      className="newQuoteBtn"
+                      onClick={() => this.componentDidMount()}
+                    >
+                      New Quote
+                    </button>
+                    <button
+                      className="favoriteBtn"
+                      onClick={() => this.addToFavorites()}
+                    >
+                      Favorite
+                    </button>
+                  </div>
+                )
+              )
+            }
+          />
+          <Route
+            exact
+            path="/favorites"
+            render={() => <Favorites favoritesArray={this.state.favorites} />}
+          />
+        </Switch>
       </main>
-         
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
