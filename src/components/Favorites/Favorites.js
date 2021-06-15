4178652bae5b34960046639bb51eaad40b56cd2c
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 21:08:30 -0400

    Format Favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import QuoteCard from '../QuoteCard/QuoteCard'
-import './Favorites.css'
+import React from "react";
+import QuoteCard from "../QuoteCard/QuoteCard";
+import "./Favorites.css";
 
-const Favorites = ( {favoritesArray} ) => {
-    const favoritesCards = favoritesArray.map((fave, index) => {
-        return (
-            <QuoteCard animeQuote={fave} key={index}/>
-        )
-    })
-    return (
-        <div className='favoritesList'>
-            {!favoritesArray.length &&
-                <h1 className='noFavorites'>You have no favorites!</h1>
-            }
-            {favoritesCards}
-        </div>
-    )
-}
-  
-  export default Favorites
+const Favorites = ({ favoritesArray }) => {
+  const favoritesCards = favoritesArray.map((fave, index) => {
+    return <QuoteCard animeQuote={fave} key={index} />;
+  });
+  return (
+    <div className="favoritesList">
+      {!favoritesArray.length && (
+        <h1 className="noFavorites">You have no favorites!</h1>
+      )}
+      {favoritesCards}
+    </div>
+  );
+};
+
+export default Favorites;
