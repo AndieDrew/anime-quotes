4cf6a205c65edee8e1cd3d3c4d2e943c61f8e4ab
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-14 21:09:43 -0400

    Format QuoteCard.js

--- src/components/QuoteCard/QuoteCard.js
+++ src/components/QuoteCard/QuoteCard.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './QuoteCard.css'
+import React from "react";
+import "./QuoteCard.css";
 
-const QuoteCard =( props ) => {
-const { anime, character, quote } = props.animeQuote
-    return (
-        <article className='card'>
-          <h1 className='animeTitle'>{anime}</h1>
-          <h2 className='animeQuote'>{quote}</h2>
-          <h2 className='animeCharacter'>- {character}</h2>
-        </article>
-    )
-  }
+const QuoteCard = (props) => {
+  const { anime, character, quote } = props.animeQuote;
+  return (
+    <article className="card">
+      <h1 className="animeTitle">{anime}</h1>
+      <h2 className="animeQuote">{quote}</h2>
+      <h2 className="animeCharacter">- {character}</h2>
+    </article>
+  );
+};
 
-export default QuoteCard
+export default QuoteCard;
